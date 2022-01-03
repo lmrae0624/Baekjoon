844434c0a6c6d4fbe11c977355d934cc99b1e397
lmrae0624 <lmrae0624@gmail.com>
2022-01-03 15:44:08 +0900

    commit

---
+++ 3. for문/A+B - 7.py
@@ -0,0 +1,4 @@
+x=int(input())
+for i in range(x):
+    a,b=map(int,input().split())
+    print("Case #"+str(i+1)+":",a+b)
---
+++ 3. for문/X보다 작은 수.py
@@ -0,0 +1,5 @@
+x, y=map(int,input().split())
+a=map(int,input().split())
+for i in a:
+    if i<y:
+        print(i, end=" ")
